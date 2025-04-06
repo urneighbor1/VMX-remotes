12ff949e931a783e6f66a9ffdb5ab2574467c3fe
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-04-06 21:24:02 +0900

    NetworkTablesでのエントリーの値の変更を監視する例を作る

--- robot.py
+++ robot.py
@@ -1,5 +1,39 @@
+import time
+
+# from cscore import CameraServer, CvSink
+from networktables import NetworkTables
+# import numpy as np
+# import cv2
+
+NetworkTables.setServerTeam(1234)
+NetworkTables.initialize(server="127.0.0.1")
+
+# CameraServer.enableLogging()
+
+
 def main():
-    print("Hello from VMX-remotes!")
+    # camera = CameraServer.startAutomaticCapture()
+    # camera.setResolution(360, 680)
+    # sink = CameraServer.getVideo()
+
+    # print(camera.isConnected())
+
+    mode_value = NetworkTables.getEntry("/Py Mode")
+    mode_value.setString("none")
+    prev_mode = mode_value.getString("none")
+
+    # input_img = cv2.Mat(np.empty((360, 680, 3)))
+    while True:
+        time.sleep(0.5)
+        mode = mode_value.getString("none")
+        if mode != prev_mode:
+            print(f"mode: {mode}")
+        prev_mode = mode
+
+        # grabbed_time, input_img = CvSink.grabFrame(sink, input_img)
+
+        # cv2.imshow("image", input_img)
+        # cv2.waitKey(1)
 
 
 if __name__ == "__main__":
