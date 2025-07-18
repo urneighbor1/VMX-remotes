964c74bed699c38a80dc7a71f95b52abb399b3de
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-07-18 15:31:48 +0900

    PCのカメラを使ってテストするためのスクリプトを追加

---
+++ test_camera.py
@@ -0,0 +1,31 @@
+"""ローカルのテスト用スクリプト"""
+from typing import cast
+
+import cv2
+
+from color_square_detector import ColorImage, ColorSquareDetector
+from config import load_preferences
+from robot import HEIGHT, WIDTH, display_results, save_image
+
+
+def main() -> None:
+    detector = ColorSquareDetector(HEIGHT, WIDTH, load_preferences())
+    cap = cv2.VideoCapture(1)
+    while True:
+        ret, frame = cap.read()
+        frame = cast("ColorImage", cv2.resize(frame, (WIDTH, HEIGHT)))
+
+        # 色付き四角形を検出
+        colored_squares = detector.detect_colored_squares(frame)
+
+        # 結果を画像に描画して表示
+        display_results(frame, detector, colored_squares)
+
+        key = cv2.waitKey(100)
+        if key == ord("s"):
+            # 元の画像を保存
+            save_image(frame)
+
+
+if __name__ == "__main__":
+    main()
